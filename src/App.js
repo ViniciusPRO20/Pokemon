160b89e53631e1c9b4373ec50055c68a19744282
vinicius <viniciuspro333@gmail.com>
2023-03-09 14:38:04 -0300

    pokemon api

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import PokeList from "./components/PokeList";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Pokémons</h1>
+      <PokeList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PokeList.js
@@ -0,0 +1,40 @@
+import "./PokeList.css";
+import { useState } from 'react';
+import axios from 'axios';
+
+function PokeSearch() {
+  const [query, setQuery] = useState('');
+  const [pokemon, setPokemon] = useState(null);
+
+  const handleSearch = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${query.toLowerCase()}`)
+      .then(response => {
+        setPokemon(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <input placeholder="Pesquise o Id ou Nome" type="text" value={query} onChange={event => setQuery(event.target.value)} />
+      <button onClick={handleSearch}>Buscar</button>
+      {pokemon && (
+        <div>
+          <h2>{pokemon.name}</h2>
+          <ul className="pokemon">
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          </ul>
+          <ul className='types'>
+            {pokemon.types.map(type => (
+              <li key={type.type.name}>{type.type.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default PokeSearch;
