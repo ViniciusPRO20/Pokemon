76bfd4221a6867c61035b63539f5b4b27fd147bf
vinicius viana <viniciuspro333@gmail.com>
2023-06-23 13:36:47 -0300

    update

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -17,23 +17,26 @@ function PokeSearch() {
   };
 
   return (
-    <div className="box">
-      <input type="text" value={query} onChange={event => setQuery(event.target.value)} />
-      <button onClick={handleSearch}>Buscar</button>
-      {pokemon && (
-        <div className="pokemons">
-          <h2>{pokemon.name}</h2>
-          <ul className="pokemon">
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-          </ul>
-          <ul className='types'>
-            {pokemon.types.map(type => (
-              <li key={type.type.name}>{type.type.name}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+    <div className="pokedex">
+      <div className="box">
+        <input type="text" value={query} onChange={event => setQuery(event.target.value)} />
+        <button onClick={handleSearch}>Buscar</button>
+        {pokemon && (
+          <div className="pokemons">
+            <h2>{pokemon.name}</h2>
+            <ul className="pokemon">
+              <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            </ul>
+            <ul className='types'>
+              {pokemon.types.map(type => (
+                <li key={type.type.name}>{type.type.name}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
+
   );
 }
 
